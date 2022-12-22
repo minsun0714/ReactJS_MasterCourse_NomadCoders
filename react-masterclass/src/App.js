4e713b217655bd3a10bd2c30d45a52d8acdc5648
minsun0714 <woooooooow22@gmail.com>
2022-12-22 18:47:51 +0900

    ✨ dark theme 설정 (ThemeProvider 활용)

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,59 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const animation = keyframes`
-  0% {
-      transform:rotate(0deg);
-      border-radius:0px;
-  }
-  50% {
-      transform:rotate(360deg);
-      border-radius:100px;
-
-  }
-  100% {
-      transform:rotate(0deg);
-      border-radius:0px;
-
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 20px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${animation} 2s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 200px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as='p'>🤩</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
