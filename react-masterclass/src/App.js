beceb0b2690aefb8436fb716fa085233729e464f
minsun0714 <woooooooow22@gmail.com>
2022-12-22 16:04:32 +0900

    ✨ props를 넘겨서 설정 변경 가능한 component 만들기

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -4,14 +4,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -23,9 +17,8 @@ const Text = styled.h1`
 function App() {
   return (
     <Father>
-      <BoxOne />
-      <Text>Hello</Text>
-      <BoxTwo />
+      <Box bgColor='teal' />
+      <Box bgColor='tomato' />
     </Father>
   );
 }
