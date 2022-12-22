71ba0927b525709563f4e3c09faffa1aba38cdb1
minsun0714 <woooooooow22@gmail.com>
2022-12-22 16:19:39 +0900

    ✨ as를 사용하여 HTML 태그 종류 변경

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -3,26 +3,20 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.h1`
-  color: black;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor='teal' />
-      <Circle bgColor='tomato' />
+    <Father as='header'>
+      <Btn>Log in</Btn>
+      <Btn as='a' href='/'>
+        Log in
+      </Btn>
     </Father>
   );
 }
