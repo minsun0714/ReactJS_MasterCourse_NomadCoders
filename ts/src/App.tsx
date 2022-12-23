8cddcb0bccd78edcc7c2af76ec1a83e175034fad
minsun0714 <woooooooow22@gmail.com>
2022-12-23 18:12:31 +0900

    ✨ event의 type 지정

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 import styled, { keyframes } from "styled-components";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+  };
   return (
     <div>
       <form>
-        <input type='text' placeholder='username' />
+        <input
+          value={value}
+          onChange={onChange}
+          type='text'
+          placeholder='username'
+        />
         <button>Log in</button>
       </form>
     </div>
