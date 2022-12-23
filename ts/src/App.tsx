895981f25fa46b055ed9a12968868c8076346e35
minsun0714 <woooooooow22@gmail.com>
2022-12-23 15:52:30 +0900

    ✨ Circle 컴포넌트 생성

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,7 +1,14 @@
 import styled, { keyframes } from "styled-components";
+import Circle from "./Circle";
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Circle bgColor='teal' />
+      <Circle bgColor='tomato' />
+      <div>hi</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ ts/src/Circle.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function Circle({ bgColor }) {
+  return <Container />;
+}
+
+export default Circle;
