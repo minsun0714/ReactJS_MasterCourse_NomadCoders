caabcccc2ffd2a36ce9a43b62c41cfaec4907eba
minsun0714 <woooooooow22@gmail.com>
2022-12-23 19:51:20 +0900

    ✨ theme의 type화

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,30 +1,18 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = React.useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello");
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type='text'
-          placeholder='username'
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
