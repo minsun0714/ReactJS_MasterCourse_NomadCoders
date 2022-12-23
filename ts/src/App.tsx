c07cf9f07ac47784e75de9616b32ae4712f8bd02
minsun0714 <woooooooow22@gmail.com>
2022-12-23 17:32:10 +0900

    ✨ borderColor default 값 설정

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -4,7 +4,7 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor='teal' />
+      <Circle borderColor='white' bgColor='teal' />
       <Circle bgColor='tomato' />
       <div>hi</div>
     </div>
--- ts/src/Circle.tsx
+++ ts/src/Circle.tsx
@@ -1,7 +1,9 @@
+import { StringifyOptions } from "querystring";
 import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,6 +11,7 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
@@ -16,8 +19,8 @@ interface CircleProps {
   borderColor?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
