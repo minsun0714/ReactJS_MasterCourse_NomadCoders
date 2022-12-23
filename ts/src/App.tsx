d53c80ed6996a992a0b3e426f480cb927df116f2
minsun0714 <woooooooow22@gmail.com>
2022-12-23 17:34:15 +0900

    💄 teal색 동그라미 테두리 yellow로 변경

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -4,7 +4,7 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle borderColor='white' bgColor='teal' />
+      <Circle borderColor='yellow' bgColor='teal' />
       <Circle bgColor='tomato' />
       <div>hi</div>
     </div>
