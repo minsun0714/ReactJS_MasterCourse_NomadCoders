c64d9396af65af5fbe9c2bde791b9c98c52db3a4
minsun0714 <woooooooow22@gmail.com>
2022-12-26 16:45:29 +0900

    🎨 Container, Header, CoinsList, Coin 컴포넌트 생성

--- ts/src/routes/Coins.tsx
+++ ts/src/routes/Coins.tsx
@@ -1,10 +1,27 @@
 import styled from "styled-components";
 
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const CoinsList = styled.header``;
+
+const Coin = styled.li``;
+
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
 function Coins() {
-  return <Title>어여쁜 코인들</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>어여쁜 코인들</Title>;
+      </Header>
+      <CoinsList>
+        <Coin></Coin>
+      </CoinsList>
+    </Container>
+  );
 }
 export default Coins;
