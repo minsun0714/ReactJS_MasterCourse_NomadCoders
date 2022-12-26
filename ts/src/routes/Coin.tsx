21ad5e35e09f3d84f5889339435b5acfe7dcbd6e
minsun0714 <woooooooow22@gmail.com>
2022-12-26 18:42:07 +0900

    🎨 Title, Loader, Container, Header 컴포넌트 복붙

--- ts/src/routes/Coin.tsx
+++ ts/src/routes/Coin.tsx
@@ -1,11 +1,44 @@
 import { useParams } from "react-router";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-itmes: center;
+`;
 
 interface RouterParams {
   coinId: string;
 }
 
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouterParams>();
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coin: {coinId}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 export default Coin;
