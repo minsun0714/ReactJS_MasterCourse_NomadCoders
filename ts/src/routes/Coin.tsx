77de792654fa3aa7e3be200c392579a2e2c2752a
minsun0714 <woooooooow22@gmail.com>
2022-12-26 15:32:01 +0900

    🎨 useParams로 현재 coin의 coinId를 보여줌

--- ts/src/routes/Coin.tsx
+++ ts/src/routes/Coin.tsx
@@ -1,4 +1,11 @@
+import { useParams } from "react-router";
+
+interface RouterParams {
+  coinId: string;
+}
+
 function Coin() {
-  return <h1>Coin</h1>;
+  const { coinId } = useParams<RouterParams>();
+  return <h1>Coin: {coinId}</h1>;
 }
 export default Coin;
