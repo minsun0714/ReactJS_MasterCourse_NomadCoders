d4e7678d8bd324f290ae83125b9c526f71f33a9f
minsun0714 <woooooooow22@gmail.com>
2022-12-27 15:38:44 +0900

    🎨 Tab과 Tabs 추가

--- ts/src/routes/Coin.tsx
+++ ts/src/routes/Coin.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Switch, Route, useParams, useLocation } from "react-router";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
@@ -46,6 +47,26 @@ const OverviewItem = styled.div`
 `;
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  a {
+    display: block;
+  }
 `;
 
 interface RouterParams {
@@ -167,6 +188,16 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+
+          <Tabs>
+            <Tab>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
           <Switch>
             <Route path={`/${coinId}/price`}>
               <Price />
