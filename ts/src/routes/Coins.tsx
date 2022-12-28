570400a5d6565930ef58e4df04c8bca7e975e57b
minsun0714 <woooooooow22@gmail.com>
2022-12-28 09:04:09 +0900

    👽️  useQuery 사용하여 API를 받아옴.

--- ts/src/routes/Coins.tsx
+++ ts/src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { fetchCoins } from "./api";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,7 +17,7 @@ const Header = styled.header`
   align-itmes: center;
 `;
 
-const CoinsList = styled.header``;
+const CoinsList = styled.ul``;
 
 const Coin = styled.li`
   background-color: white;
@@ -24,7 +26,7 @@ const Coin = styled.li`
   margin-bottom: 10px;
   list-style: none;
   a {
-    display: flex;
+    display: block;
     align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
@@ -46,7 +48,7 @@ const Loader = styled.span`
   display: block;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -63,26 +65,17 @@ const Img = styled.img`
 `;
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
       <Header>
         <Title>코인들</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
-          {coins.map((coin) => (
+          {data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link
                 to={{
