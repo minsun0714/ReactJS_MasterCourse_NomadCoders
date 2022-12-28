7518aafb0872f103be0ecbe1a6b4d394bd551fdf
minsun0714 <woooooooow22@gmail.com>
2022-12-28 20:45:14 +0900

    ✨ ApexChart 추가

--- ts/src/routes/Chart.tsx
+++ ts/src/routes/Chart.tsx
@@ -1,14 +1,29 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
+
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>{isLoading ? "Loading chart..." : <ApexChart type='line' />}</div>
+  );
 }
 
 export default Chart;
