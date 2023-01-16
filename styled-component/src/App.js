55e12c27c894363be801b320a6309fc22ecec383
minsun0714 <woooooooow22@gmail.com>
2023-01-16 19:44:13 +0900

    💄 Box에 rotation animation 적용

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -4,10 +4,29 @@ const Wrapper = styled.div`
   display: flex;
 `;
 
+const rotation = keyframes`
+    0% {
+      transform:rotate(0deg);
+      border-radius: 0px;
+      background-color: green;
+    }
+    50% {
+      transform:rotate(360deg);
+      border-radius: 100px;
+      background-color: aliceblue;
+    }
+    100% {
+      transform:rotate(0deg);
+      border-radius: 0px;
+      background-color: green;
+    }
+`;
+
 const Box = styled.div`
   width: 200px;
   height: 200px;
   background-color: tomato;
+  animation: ${rotation} 1s linear infinite;
 `;
 
 function App() {
