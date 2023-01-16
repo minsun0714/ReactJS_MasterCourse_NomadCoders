0a830a6c8b8535de09ebd9aab47af26b07c8c8b1
minsun0714 <woooooooow22@gmail.com>
2023-01-16 18:09:26 +0900

    ✨ styled-components 이용하여 2개의 box 생성

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
-function App() {}
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.h1`
+  color: skyblue;
+`;
+
+function App() {
+  return (
+    <Father>
+      <BoxOne>
+        <Text>hi</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
+}
 
 export default App;
