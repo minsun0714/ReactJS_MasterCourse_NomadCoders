4c61b157de09ecb7e6d5134515d6cbb1c144f069
minsun0714 <woooooooow22@gmail.com>
2023-01-17 21:35:18 +0900

    🧑‍💻 DummyProp 생성 및 review

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -7,7 +7,29 @@ const Wrapper = styled.div`
   background-color: ${(prop) => prop.theme.bgColor};
 `;
 
+interface H1Props {
+  text: string;
+}
+
+const H1 = styled.h1<H1Props>`
+  color: ${(prop) => prop.theme.textColor};
+`;
+
+interface DummyProps {
+  text?: string;
+  active?: boolean;
+}
+
+function Dummy({ text = "안녕하시렵니까", active = false }: DummyProps) {
+  return (
+    <div>
+      <H1 text={text}>{text}</H1>
+    </div>
+  );
+}
+
 function App() {
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {};
   const [value, setValue] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
@@ -21,6 +43,8 @@ function App() {
   };
   return (
     <Wrapper>
+      <Dummy active={true} text='Minsun' />
+      <button onClick={onclick}>click me</button>
       <Button bgColor='violet' />
       <Circle bgColor='yellow' />
       <Circle bgColor='tomato' text='hihi' />
