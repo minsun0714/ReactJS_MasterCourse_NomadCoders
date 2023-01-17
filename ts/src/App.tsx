2387ccec8a93b11163557039efaa985e28fdc8f0
minsun0714 <woooooooow22@gmail.com>
2023-01-17 20:17:48 +0900

    🧑‍💻 review(input & button 구현)

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Circle from "./Circle";
+import Button from "./Button";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setValue("");
+  };
   return (
     <div>
+      <Button bgColor='violet' />
       <Circle bgColor='yellow' />
       <Circle bgColor='tomato' text='hihi' />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type='text'
+          placeholder='username'
+        ></input>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
---
+++ ts/src/Button.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface Buttonprops {
+  bgColor: string;
+}
+
+const Button = styled.button<Buttonprops>`
+  width: 100px;
+  height: 50px;
+  background-color: ${(prop) => prop.bgColor};
+  border: 0px;
+`;
+
+interface Btnprops {
+  bgColor: string;
+}
+
+function Btn({ bgColor }: Btnprops) {
+  const [count, setCount] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setCount(event.currentTarget.value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setCount("");
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <input placeholder='practice' value={count} onChange={onChange}></input>
+      <Button bgColor={bgColor}>hi</Button>
+    </form>
+  );
+}
+export default Btn;
