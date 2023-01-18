e18f7ae8f11f18c55bcecabbc762c35134cb72ee
minsun0714 <woooooooow22@gmail.com>
2023-01-18 19:17:04 +0900

    ⚡️ Coins, Coin Route 지정

--- cryp/src/Router.tsx
+++ cryp/src/Router.tsx
@@ -1,4 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Coins from "./routes/Coins";
+import Coin from "./routes/Coin";
 
-function Router() {}
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/:coinId'>
+          <Coin />
+        </Route>
+        <Route path='/'>
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 export default Router;
