2072a5147c15e4be317c34f5eac47685c4fe643e
minsun0714 <woooooooow22@gmail.com>
2023-01-18 17:12:03 +0900

    🎉 crypto-tracker review 시작

---
+++ crypto-tracker_review/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  textColor: "whitesmoke",
+  bgColor: "black",
+  accentColor: "#575fcf",
+};
+
+export const lightTheme: DefaultTheme = {
+  textColor: "black",
+  bgColor: "whitesmoke",
+  accentColor: "#575fcf",
+};
