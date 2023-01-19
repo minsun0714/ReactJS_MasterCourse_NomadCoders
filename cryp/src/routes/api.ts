b281111b9e385c2834565eb091f2378a05f8aea3
minsun0714 <woooooooow22@gmail.com>
2023-01-19 22:40:24 +0900

    👽️ fetchCoinInfo, fetchCoinTickers 생성

--- cryp/src/routes/api.ts
+++ cryp/src/routes/api.ts
@@ -3,3 +3,13 @@ export function fetchCoins() {
     response.json()
   );
 }
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`).then(
+    (response) => response.json()
+  );
+}
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`).then(
+    (response) => response.json()
+  );
+}
