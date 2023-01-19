8d12559df1747f66ee8fc907769b94dde386e703
minsun0714 <woooooooow22@gmail.com>
2023-01-19 16:48:24 +0900

    ✨ useLocation 사용하여 Coins 내 Link tag에 입력한 state 받음

--- cryp/src/routes/Coin.tsx
+++ cryp/src/routes/Coin.tsx
@@ -1,13 +1,58 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useParams, useLocation } from "react-router";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Title = styled.h1`
+  color: black;
+  font-size: 48px;
+`;
+
+const Loading = styled.h1`
+  margin-top: 50px;
+  font-size: 50px;
+  color: ${(props) => props.theme.accentColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px 20px;
+  margin: auto;
+  max-width: 480px;
+`;
+
+const Header = styled.header`
+  color: ${(props) => props.theme.bgColor};
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  console.log(coinId);
-  return <div>Coin: {coinId}는 짱짱 코인</div>;
+  const { state } = useLocation<RouteState>();
+  console.log(state?.name ?? "Loading");
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name ?? "Loading"}</Title>
+      </Header>
+      {loading ? <Loading>Loading..</Loading> : null}
+    </Container>
+  );
 }
 export default Coin;
