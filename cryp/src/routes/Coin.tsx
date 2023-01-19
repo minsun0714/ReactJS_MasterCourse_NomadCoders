da3abe859310b722ebbc93fea08d6a788e01cfae
minsun0714 <woooooooow22@gmail.com>
2023-01-19 19:08:43 +0900

    🎨 Coin detail 정보 구현된 코드 이용

--- cryp/src/routes/Coin.tsx
+++ cryp/src/routes/Coin.tsx
@@ -33,6 +33,31 @@ const Header = styled.header`
   margin-bottom: 20px;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+  color: whitesmoke;
+  text-align: center;
+`;
+
 interface RouteParams {
   coinId: string;
 }
@@ -122,9 +147,41 @@ function Coin() {
   return (
     <Container>
       <Header>
-        <Title>{state?.name ?? "Loading"}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading" : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loading>Loading..</Loading> : null}
+      {loading ? (
+        <Loading>Loading..</Loading>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 }
