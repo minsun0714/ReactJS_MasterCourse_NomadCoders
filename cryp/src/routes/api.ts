cbf66904ae12e8ecfb9c4c52088308b20dbc0ebd
minsun0714 <woooooooow22@gmail.com>
2023-01-20 22:01:05 +0900

    👽️ error 발생 (원인 불분명)=> API 변경

--- cryp/src/routes/api.ts
+++ cryp/src/routes/api.ts
@@ -14,7 +14,9 @@ export function fetchCoinTickers(coinId: string) {
   );
 }
 export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 23;
   return fetch(
-    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+    `https://api.coinpaprika.com/v1/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
   ).then((response) => response.json());
 }
