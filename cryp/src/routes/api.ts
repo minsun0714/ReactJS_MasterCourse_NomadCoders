70b2da2f6195bb195d003722fcf21a68ba0ff940
minsun0714 <woooooooow22@gmail.com>
2023-01-20 16:56:47 +0900

    👽️ nomadcoders 자체 API로 변경

--- cryp/src/routes/api.ts
+++ cryp/src/routes/api.ts
@@ -14,9 +14,7 @@ export function fetchCoinTickers(coinId: string) {
   );
 }
 export function fetchCoinHistory(coinId: string) {
-  const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 23;
   return fetch(
-    `https://api.coinpaprika.com/v1/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
   ).then((response) => response.json());
 }
