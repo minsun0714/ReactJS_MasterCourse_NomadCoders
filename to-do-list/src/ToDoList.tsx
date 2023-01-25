47367317f0a86cdd122d5302a0c36bf90ebbce3a
minsun0714 <woooooooow22@gmail.com>
2023-01-25 17:06:09 +0900

    ✨ useForm 사용하여 login form 구현

--- to-do-list/src/ToDoList.tsx
+++ to-do-list/src/ToDoList.tsx
@@ -1,21 +1,71 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setTodo] = useState("");
+//   const [toDoError, setTodoError] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setTodoError("write more than 10");
+//     }
+//     setTodoError("");
+//     console.log("submit");
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={toDo} placeholder='Write a to do' />
+//         <button>Add</button>
+//         {toDoError}
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [toDo, setTodo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
+  const { register, watch, handleSubmit } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setTodo("");
-  };
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder='Write a to do' />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...(register("Email"), { required: true })}
+          required
+          placeholder='Email'
+        />{" "}
+        <input
+          {...(register("firstName"), { required: true })}
+          placeholder='firstName'
+        />{" "}
+        <input
+          {...(register("lastName"), { required: true })}
+          placeholder='lastName'
+        />{" "}
+        <input
+          {...(register("userName"), { required: true, minLength: 10 })}
+          placeholder='userName'
+        />{" "}
+        <input
+          {...(register("pw"), { required: true, minLength: 5 })}
+          placeholder='pw'
+        />
+        <input
+          {...(register("pwConfirmation"), { required: true, minLength: 5 })}
+          placeholder='pwConfirmation'
+        />{" "}
         <button>Add</button>
       </form>
     </div>
